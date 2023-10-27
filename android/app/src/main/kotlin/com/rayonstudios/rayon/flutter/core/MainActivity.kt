dff81ccec7f057cac46fa845eda1fbb3173e8ced
Talha Javed Khan <talhakhan1297@yahoo.com>
2023-10-27 19:31:21 +0500

    Added Basic Project files

---
+++ android/app/src/main/kotlin/com/rayonstudios/rayon/flutter/core/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rayonstudios.rayon_flutter_core
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:rayon_flutter_core/app/app.dart';
+import 'package:rayon_flutter_core/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
